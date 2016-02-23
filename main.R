250877c0ddf4f9b7872a2f96343664694c852b83
Odin <odinuv@gmail.com>
2016-02-23 19:33:13 +0100

    fix: proper function

--- main.R
+++ main.R
@@ -1,4 +1,4 @@
-devtools::install_local('/home/')
+devtools::install('/home/')
 library(keboola.r.custom.application.subclass)
 app <- CustomApplicationExample$new(Sys.getenv("KBC_DATA_DIR"))
 app$run()
